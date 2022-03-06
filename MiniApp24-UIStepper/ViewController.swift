47c42c9962ad955d5a543f44561dbdedd98b22e5
MaedaKouta <maedakoutaa@gmail.com>
2022-03-06 18:29:27 +0900

    Initial Commit

---
+++ MiniApp24-UIStepper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp24-UIStepper
+//
+//  Created by 前田航汰 on 2022/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
